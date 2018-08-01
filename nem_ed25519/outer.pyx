73e7e9340416ca44923781377bd5f7afd4566869
namuyan <thhjuu@yahoo.co.jp>
2018-08-01 23:48:24 +0900

    change def->cdef

--- nem_ed25519/outer.pyx
+++ nem_ed25519/outer.pyx
@@ -4,27 +4,29 @@ Point = namedtuple('Point', ['x', 'y'])
 
 PRIME = PRIME = 2 ** 255 - 19
 
-def inverse(x):
+cdef inverse(x):
     return pow(x, PRIME - 2, PRIME)
 
 
 D = -121665 * inverse(121666) % PRIME
 
 
+cdef _inner(px, py, qx, qy):
+    x = (px * qy + qx * py) * inverse(1 + D * px * qx * py * qy)
+    y = (py * qy + px * qx) * inverse(1 - D * px * qx * py * qy)
+    return x % PRIME, y % PRIME
+
+
+cdef _outer(px, py, _n):
+    if _n == 0:
+        return 0, 1
+    qx, qy = _outer(px, py, _n // 2)
+    qx, qy = _inner(qx, qy, qx, qy)
+    if _n & 1:
+        qx, qy = _inner(qx, qy, px, py)
+    return qx, qy
+
+
 def outer(P, n):
-    def _inner(px, py, qx, qy):
-        x = (px * qy + qx * py) * inverse(1 + D * px * qx * py * qy)
-        y = (py * qy + px * qx) * inverse(1 - D * px * qx * py * qy)
-        return x % PRIME, y % PRIME
-
-    def _outer(px, py, _n):
-        if _n == 0:
-            return 0, 1
-        qx, qy = _outer(px, py, _n // 2)
-        qx, qy = _inner(qx, qy, qx, qy)
-        if _n & 1:
-            qx, qy = _inner(qx, qy, px, py)
-        return qx, qy
-
-    _qx, _qy = _outer(P.x, P.y, n)
-    return Point(_qx, _qy)
+    qx, qy = _outer(P.x, P.y, n)
+    return Point(qx, qy)
